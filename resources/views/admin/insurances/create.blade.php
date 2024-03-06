e2ff47359bc4fed685503dd1ab33f842ab0f7e94
PaablooDR <pab25702@gmail.com>
2024-03-06 20:37:55 +0100

    crud insurance and sponsor

--- resources/views/admin/insurances/create.blade.php
+++ resources/views/admin/insurances/create.blade.php
@@ -1,16 +1,52 @@
-<form class="container">
-  <div class="mb-3">
-    <label for="exampleInputEmail1" class="form-label">Email address</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-    <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+<div class="container">
+  <div class="row mt-4 mb-2">
+    <div class="col-md-10">
+        <h1>NEW INSURANCE</h1>
+    </div>
+    <div class="col-md-2 d-flex align-items-center justify-content-end">
+        <a href="{{ route('insurance.index') }}" class="btn btn-danger">Cancel</a>
+    </div>
   </div>
-  <div class="mb-3">
-    <label for="exampleInputPassword1" class="form-label">Password</label>
-    <input type="password" class="form-control" id="exampleInputPassword1">
-  </div>
-  <div class="mb-3 form-check">
-    <input type="checkbox" class="form-check-input" id="exampleCheck1">
-    <label class="form-check-label" for="exampleCheck1">Check me out</label>
-  </div>
-  <button type="submit" class="btn btn-primary">Submit</button>
-</form>
+  @if ($errors->any())
+    <div class="alert alert-danger">
+      <ul>
+        @foreach ($errors->all() as $error)
+          <li>{{ $error }}</li>
+        @endforeach
+      </ul>
+    </div>
+  @endif
+  <form action="{{ route('insurance.store') }}" method="POST" enctype="multipart/form-data" class="mb-3">
+    @csrf
+    <div class="mb-3">
+      <label for="cif" class="form-label">CIF</label>
+      <input type="text" class="form-control" id="cif" name="cif" aria-describedby="cif-help">
+      <div id="cif-help" class="form-text">Introduce the CIF of the insurance.</div>
+    </div>
+    <div class="mb-3">
+      <label for="name" class="form-label">Name</label>
+      <input type="text" class="form-control" id="name" name="name" aria-describedby="name-help">
+      <div id="name-help" class="form-text">Introduce the name of the insurance.</div>
+    </div>
+    <div class="mb-3">
+      <label for="address" class="form-label">Address</label>
+      <input type="text" class="form-control" id="address" name="address" aria-describedby="address-help">
+      <div id="address-help" class="form-text">Introduce the insurance's address</div>
+    </div>
+    <div class="mb-3">
+      <label for="price" class="form-label">Price</label>
+      <input type="number" class="form-control" id="price" name="price" aria-describedby="price-help" required>
+      <div id="price-help" class="form-text">Set up the insurance's price.</div>
+    </div>
+    <div class="mb-3">
+      <label for="active" class="form-label">Active</label>
+      <select id="active" name="active" aria-describedby="active-help" class="form-select" aria-label="Select if the insurance is active or not" required>
+        <option value="1" selected>Yes</option>
+        <option value="0">No</option>
+      </select>
+      <div id="active-help" class="form-text">Is the insurance active?</div>
+    </div>
+
+    <button type="submit" class="btn btn-primary">Submit</button>
+  </form>
+</div>
--- routes/web.php
+++ routes/web.php
@@ -32,9 +32,13 @@ Route::get('admin/sponsors', [SponsorController::class, 'index'])->name('sponsor
 Route::get('sponsor/create', [SponsorController::class, 'create'])->name('sponsor.create');
 Route::get('sponsor/edit/{sponsor}', [SponsorController::class, 'edit'])->name('sponsor.edit');
 Route::put('sponsor/update{sponsor}', [SponsorController::class, 'update'])->name('sponsor.update');
+Route::post('sponsor/store', [SponsorController::class, 'store'])->name('sponsor.store'); 
 
 Route::get('admin/insurances', [InsuranceController::class, 'index'])->name('insurance.index');
 Route::get('insurance/create', [InsuranceController::class, 'create'])->name('insurance.create');
+Route::get('insurance/edit/{insurance}', [InsuranceController::class, 'edit'])->name('insurance.edit');
+Route::put('insurance/update{insurance}', [InsuranceController::class, 'update'])->name('insurance.update');
+Route::post('insurance/store', [InsuranceController::class, 'store'])->name('insurance.store'); 
 
 Route::get('admin/challenges', [ChallengeController::class, 'index'])->name('challenge.index');
 Route::get('challenge/create', [ChallengeController::class, 'create'])->name('challenge.create');
