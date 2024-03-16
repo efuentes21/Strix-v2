7611278aed4b1518bb2a6129c3631a81ee35832e
efuentes21 <91967192+efuentes21@users.noreply.github.com>
2024-03-16 21:13:10 +0100

    mobile thumbnail

--- resources/views/user/components/banner-image.blade.php
+++ resources/views/user/components/banner-image.blade.php
@@ -1,3 +1,7 @@
 <div class="banner-container">
-    <img src="{{ asset('resources/mainpage-thumbnail3.jpg') }}" class="img-fluid" alt="Racing challenge banner example">
+    <div class="banner-img-desktop"></div>
+    <!-- <img src="{{ asset('resources/mainpage-thumbnail3.jpg') }}" class="img-fluid banner-img-desktop" alt="Racing challenge banner example"> -->
+    <div class="banner-img-mobile"></div>
+
+    <!-- <img src="{{ asset('resources/banner-thumbnail-mobile.png') }}" class="img-fluid banner-img-mobile" alt="Racing challenge banner example"> -->
 </div>
