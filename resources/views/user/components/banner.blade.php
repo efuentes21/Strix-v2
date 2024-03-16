654f8cc2ad40ba30c57f39ffd075c61b740a388c
efuentes21 <91967192+efuentes21@users.noreply.github.com>
2024-03-16 21:24:30 +0100

    movile banner responsive

--- resources/views/user/components/banner.blade.php
+++ resources/views/user/components/banner.blade.php
@@ -1,7 +1,7 @@
 <div class="p-5 mt-xxl-7 mt-md-5 mt-sm-3 mb-4 rounded-3">
     <div class="container-fluid py-5">
-        <h1 class="col-md-7 display-5 fw-bold text-white text-outline-1 text-title-banner">ACCEPT THE CHALLENGE</h1>
-        <p class="col-md-7 fs-4 text-white text-outline-1">Unleash your inner champion and conquer our races! Join the journey where determination meets triumph, and let every obstacle become a stepping stone to your success.</p>
+        <h1 class="col-md-7 display-5 fw-bold text-white text-outline-1 text-title-banner text-sm">ACCEPT THE CHALLENGE</h1>
+        <p class="col-md-7 fs-4 text-white text-outline-1">Unleash your inner champion and conquer our races! <span class="text-banner-subtitle">Join the journey where determination meets triumph, and let every obstacle become a stepping stone to your success.<span></p>
         <div class="flex g-1">
             <a href="{{ route('/') }}" class="btn btn-danger btn-lg">JOIN NOW!</a>
             <a href="{{ route('/') }}" class="btn btn-dark btn-lg">FIND YOUR RACE</a>
