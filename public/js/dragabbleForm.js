1d0f009dd7a31e21cccb83bf64598eaedc232087
efuentes21 <91967192+efuentes21@users.noreply.github.com>
2024-04-01 21:15:27 +0200

    dad

--- public/js/dragabbleForm.js
+++ public/js/dragabbleForm.js
@@ -11,11 +11,36 @@ $(document).ready(function() {
         var files = e.originalEvent.dataTransfer.files;
         var formData = new FormData();
 
+        formData.append('_token', $('input[name="_token"]'))
+
         for (var i = 0; i < files.length; i++) {
             console.log(files[i]);
             formData.append('images[]', files[i]);
         }
 
-        console.log($('input[name="images[]"]').val());
+        var currentUrl = window.location.href;
+        var urlParts = currentUrl.split('/');
+        var raceId = urlParts[urlParts.length - 1];
+        var url = '/admin/raceimages/store/' + raceId;
+        console.log(url)
+
+        $.ajax({
+            url: url,
+            type: 'POST',
+            data: formData,
+            processData: false,
+            contentType: false,
+            success: function(response) {
+                console.log('Archivos subidos correctamente');
+                // Aquí puedes manejar la respuesta del servidor si es necesario
+            },
+            error: function(xhr, status, error) {
+                console.error('Error al subir archivos:', error);
+                // Manejar errores aquí
+            }
+        });
+        for (var pair of formData.entries()) {
+            console.log(pair[0] + ', ' + pair[1]);
+        }
     });
 });
